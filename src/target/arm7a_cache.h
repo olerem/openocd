51d4e772cf1d0ef941e9cc79bace2ec3718d1189
Oleksij Rempel <linux@rempel-privat.de>
2015-04-23 07:49:13 +0200

    add arm7a_cache
    Change-Id: I849f4d1f20610087885eeddefa81d976f77cf199
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

---
+++ src/target/arm7a_cache.h
@@ -0,0 +1,23 @@
+/***************************************************************************
+ *   Copyright (C) 2015 Oleksij Rempel                                     *
+ *   linux@rempel-privat.de                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ ***************************************************************************/
+
+#ifndef ARM7A_CACHE_H
+#define ARM7A_CACHE_H
+
+#include "arm_jtag.h"
+
+extern const struct command_registration arm7a_cache_command_handlers[];
+
+#endif
