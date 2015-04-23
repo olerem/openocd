86f725926952d58b0f50a197fd9ca2a45943a241
Oleksij Rempel <linux@rempel-privat.de>
2015-04-23 07:49:13 +0200

    add arm7a_cache handlers
    This patch introduces, new command set and handlers for l1 and l2x caches.
    
    Change-Id: I849f4d1f20610087885eeddefa81d976f77cf199
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- src/target/armv7a.h
+++ src/target/armv7a.h
@@ -21,6 +21,7 @@
 #define ARMV7A_H
 
 #include "arm_adi_v5.h"
+#include "armv7a_cache.h"
 #include "arm.h"
 #include "armv4_5_mmu.h"
 #include "armv4_5_cache.h"
@@ -68,6 +69,8 @@ struct armv7a_cache_common {
 	struct armv7a_cachesize i_size;		/* instruction cache */
 	int i_cache_enabled;
 	int d_u_cache_enabled;
+	int auto_cache_enabled;			/* openocd automatic
+						 * cache handling */
 	/* l2 external unified cache if some */
 	void *l2_cache;
 	int (*flush_all_data_cache)(struct target *target);
