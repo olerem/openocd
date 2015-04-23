9631ce5fa609a16d7cffb9e4527b17282653310e
Oleksij Rempel <linux@rempel-privat.de>
2015-04-23 13:15:07 +0200

    src/target/arm7a_cache_l2x.c
    Change-Id: I548125852a3cd7c5197bf1ef4bc0e98150b8e091
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- src/target/arm7a_cache_l2x.c
+++ src/target/arm7a_cache_l2x.c
@@ -28,7 +28,7 @@
 #define L2X0_CLEAN_INV_WAY              0x7FC
 
 /* L2 is not specific to armv7a  a specific file is needed */
-static int arm7a_l2x_flush_all_data(struct target *target)
+static int arm7a_l2x_inval_all(struct target *target)
 {
 	struct armv7a_common *armv7a = target_to_armv7a(target);
 	struct armv7a_l2x_cache *l2x_cache = (struct armv7a_l2x_cache *)
@@ -71,11 +71,11 @@ COMMAND_HANDLER(arm7a_l2x_cache_info_command)
 			&armv7a->armv7a_mmu.armv7a_cache);
 }
 
-COMMAND_HANDLER(arm7a_l2x_cache_flash_all_command)
+COMMAND_HANDLER(arm7a_l2x_cache_inval_all_command)
 {
 	struct target *target = get_current_target(CMD_CTX);
 
-	return arm7a_l2x_flush_all_data(target);
+	return arm7a_l2x_inval_all(target);
 }
 
 static const struct command_registration arm7a_l2x_cache_commands[] = {
@@ -87,10 +87,10 @@ static const struct command_registration arm7a_l2x_cache_commands[] = {
 		.usage = "",
 	},
 	{
-		.name = "flash_all",
-		.handler = arm7a_l2x_cache_flash_all_command,
+		.name = "inval_all",
+		.handler = arm7a_l2x_cache_inval_all_command,
 		.mode = COMMAND_ANY,
-		.help = "flash complete l2x cache",
+		.help = "invalidate complete l2x cache",
 		.usage = "",
 	},
 	COMMAND_REGISTRATION_DONE
