c0fb7be80b2af54022b0b11275fcba1439e7888a
Oleksij Rempel <linux@rempel-privat.de>
2015-05-27 16:39:33 +0200

    src/target/arm7a_cache.c
    Change-Id: I8b8f4ab01b3fff81b776332f503dad3b46b1a0b8
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- src/target/arm7a_cache.c
+++ src/target/arm7a_cache.c
@@ -22,6 +22,52 @@
 #include "armv7a.h"
 #include "arm7a_cache.h"
 #include <helper/time_support.h>
+#include "arm_opcodes.h"
+
+static int armv7a_d_cache_clean_inval_all(struct target *target)
+{
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	struct arm_dpm *dpm = armv7a->arm.dpm;
+	struct armv7a_cachesize *d_u_size =
+		&(armv7a->armv7a_mmu.armv7a_cache.d_u_size);
+	int32_t c_way, c_index = d_u_size->index;
+	int retval;
+
+	/*  check that cache data is on at target halt */
+	if (!armv7a->armv7a_mmu.armv7a_cache.d_u_cache_enabled) {
+		LOG_INFO("flushed not performed :cache not on at target halt");
+		return ERROR_OK;
+	}
+
+	retval = dpm->prepare(dpm);
+	if (retval != ERROR_OK)
+		goto done;
+
+	do {
+		c_way = d_u_size->way;
+		do {
+			uint32_t value = (c_index << d_u_size->index_shift)
+				| (c_way << d_u_size->way_shift);
+			/*  DCCISW */
+			/* LOG_INFO ("%d %d %x",c_way,c_index,value); */
+			retval = dpm->instr_write_data_r0(dpm,
+					ARMV4_5_MCR(15, 0, 0, 7, 14, 2),
+					value);
+			if (retval != ERROR_OK)
+				goto done;
+			c_way -= 1;
+		} while (c_way >= 0);
+		c_index -= 1;
+	} while (c_index >= 0);
+
+	return retval;
+
+done:
+	LOG_ERROR("clean invalidate failed");
+	dpm->finish(dpm);
+
+	return retval;
+}
 
 COMMAND_HANDLER(arm7a_cache_info_command)
 {
@@ -35,10 +81,8 @@ COMMAND_HANDLER(arm7a_cache_info_command)
 COMMAND_HANDLER(arm7a_cache_flash_all_command)
 {
 	struct target *target = get_current_target(CMD_CTX);
-	struct armv7a_common *armv7a = target_to_armv7a(target);
 
-	if (armv7a->armv7a_mmu.armv7a_cache.flush_all_data_cache)
-		armv7a->armv7a_mmu.armv7a_cache.flush_all_data_cache(target);
+	armv7a_d_cache_clean_inval_all(target);
 
 	return 0;
 }
