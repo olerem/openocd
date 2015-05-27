5a863607d06421e7a4aa26991b5fb47ead11b0da
Oleksij Rempel <linux@rempel-privat.de>
2015-05-27 17:04:02 +0200

    src/target/arm7a_cache.c
    Change-Id: I9892e2eb9f4dd535fd367acf20d112ca1468247c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- src/target/arm7a_cache.c
+++ src/target/arm7a_cache.c
@@ -69,6 +69,65 @@ done:
 	return retval;
 }
 
+static int armv7a_d_cache_inval_virt(struct target *target, uint32_t virt)
+{
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	struct arm_dpm *dpm = armv7a->arm.dpm;
+	int retval;
+
+	/*  check that cache data is on at target halt */
+	if (!armv7a->armv7a_mmu.armv7a_cache.d_u_cache_enabled) {
+		LOG_INFO("flushed not performed :cache not on at target halt");
+		return ERROR_OK;
+	}
+
+	retval = dpm->prepare(dpm);
+	if (retval != ERROR_OK)
+		goto done;
+
+	retval = dpm->instr_write_data_r0(dpm,
+				ARMV4_5_MCR(15, 0, 0, 7, 6, 1), virt);
+	if (retval != ERROR_OK)
+		goto done;
+	return retval;
+
+done:
+	LOG_ERROR("d-cache invalidate failed");
+	dpm->finish(dpm);
+
+	return retval;
+}
+
+static int armv7a_d_cache_clean_virt(struct target *target, uint32_t virt)
+{
+	struct armv7a_common *armv7a = target_to_armv7a(target);
+	struct arm_dpm *dpm = armv7a->arm.dpm;
+	int retval;
+
+	/*  check that cache data is on at target halt */
+	if (!armv7a->armv7a_mmu.armv7a_cache.d_u_cache_enabled) {
+		LOG_INFO("flushed not performed :cache not on at target halt");
+		return ERROR_OK;
+	}
+
+	retval = dpm->prepare(dpm);
+	if (retval != ERROR_OK)
+		goto done;
+
+	/* FIXME: do we need DCCVAC or DCCVAU */
+	retval = dpm->instr_write_data_r0(dpm,
+				ARMV4_5_MCR(15, 0, 0, 7, 10, 1), virt);
+	if (retval != ERROR_OK)
+		goto done;
+	return retval;
+
+done:
+	LOG_ERROR("d-cache invalidate failed");
+	dpm->finish(dpm);
+
+	return retval;
+}
+
 COMMAND_HANDLER(arm7a_cache_info_command)
 {
 	struct target *target = get_current_target(CMD_CTX);
@@ -87,6 +146,32 @@ COMMAND_HANDLER(arm7a_cache_flash_all_command)
 	return 0;
 }
 
+COMMAND_HANDLER(arm7a_cache_inval_virt_command)
+{
+	struct target *target = get_current_target(CMD_CTX);
+	uint32_t virt;
+
+	if (CMD_ARGC != 1)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], virt);
+
+	return armv7a_d_cache_inval_virt(target, virt);
+}
+
+COMMAND_HANDLER(arm7a_cache_clean_virt_command)
+{
+	struct target *target = get_current_target(CMD_CTX);
+	uint32_t virt;
+
+	if (CMD_ARGC != 1)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], virt);
+
+	return armv7a_d_cache_clean_virt(target, virt);
+}
+
 static const struct command_registration arm7a_cache_commands[] = {
 	{
 		.name = "info",
@@ -99,8 +184,22 @@ static const struct command_registration arm7a_cache_commands[] = {
 		.name = "flash_all",
 		.handler = arm7a_cache_flash_all_command,
 		.mode = COMMAND_ANY,
-		.help = "clean and invalidate all l1 caches",
+		.help = "flash (clean and invalidate) all l1 caches",
 		.usage = "",
+	},
+	{
+		.name = "inval",
+		.handler = arm7a_cache_inval_virt_command,
+		.mode = COMMAND_ANY,
+		.help = "invalidate l1 by virtual address",
+		.usage = "[virt_addr]",
+	},
+	{
+		.name = "clean",
+		.handler = arm7a_cache_clean_virt_command,
+		.mode = COMMAND_ANY,
+		.help = "clean l1 by virtual address",
+		.usage = "[virt_addr]",
 	},
 	{
 		.chain = arm7a_l2x_cache_command_handler,
