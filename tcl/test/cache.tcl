75cb492057b36fbda22fa245f55fe1f64a1e6179
Oleksij Rempel <linux@rempel-privat.de>
2015-08-12 12:59:40 +0200

    tcl/test/cache.tcl
    Change-Id: I20716d9c0c938ee9973f6eab8ad2dfa1ba5355dc
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- tcl/test/cache.tcl
+++ tcl/test/cache.tcl
@@ -9,34 +9,32 @@ proc mww_with_test {reg val} {
 	}
 }
 
-proc my_clean_test {} {
-	set def_addr 0x805cabd0
+proc cache_inval_test {reg} {
 	set pattern_1 0xaaaaaaaa
 	set pattern_2 0x55555555
 	set tmp 0
-	set targetname [target current]
 
 	halt
 	# Disable auto cache handling
 	cache auto 0
 
 	# Write down some pattern and make sure it will go to the RAM
-	mww_with_test $def_addr $pattern_1
-	if [ catch { cache l1 d clean $def_addr } msg ] {
+	mww_with_test $reg $pattern_1
+	if [ catch { cache l1 d clean $reg } msg ] {
 		return "ERROR: clean l1 cache"
 	}
-	if [ catch { cache l2x clean $def_addr } msg ] {
+	if [ catch { cache l2x clean $reg } msg ] {
 		return "ERROR: clean l2x cache"
 	}
 
-	if [ catch { cache l1 d inval $def_addr } msg ] {
+	if [ catch { cache l1 d inval $reg } msg ] {
 		return "ERROR: inval l1 inval"
 	}
-	if [ catch { cache l2x inval $def_addr } msg ] {
+	if [ catch { cache l2x inval $reg } msg ] {
 		return "ERROR: inval l2x inval"
 	}
 
-	set tmp [mrw $def_addr]
+	set tmp [mrw $reg]
 	if { $pattern_1 != $tmp } {
 		echo [format "ERROR. Write to RAM over CPU. Got: 0x%08x, expected: 0x%08x" $tmp $pattern_1]
 	} else {
@@ -44,23 +42,23 @@ proc my_clean_test {} {
 	}
 
 	# Write other patter. 
-	mww_with_test $def_addr $pattern_2
+	mww_with_test $reg $pattern_2
 
-	if [ catch { cache l1 d inval $def_addr } msg ] {
+	if [ catch { cache l1 d inval $reg } msg ] {
 		return "ERROR: clean l1 inval"
 	}
-	# TODO, currently l1 d inval is not working. Fix it!!!!
-	set tmp [mrw $def_addr]
+	# TODO, currently l1 d inval fails working. Fix it!!!!
+	set tmp [mrw $reg]
 	if { $pattern_1 != $tmp } {
 		echo [format "ERROR. Inval l1. Got: 0x%08x, expected: 0x%08x" $tmp $pattern_1]
 	} else {
 		echo "OK   . Inval l1"
 	}
 
-	if [ catch { cache l2x inval $def_addr } msg ] {
+	if [ catch { cache l2x inval $reg } msg ] {
 		return "ERROR: clean l2x inval"
 	}
-	set tmp [mrw $def_addr]
+	set tmp [mrw $reg]
 	if { $pattern_1 != $tmp } {
 		echo [format "ERROR. Inval l2x. Got: 0x%08x, expected: 0x%08x" $tmp $pattern_1]
 	} else {
